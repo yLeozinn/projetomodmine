11fe42b9bca30c03e093ee92b8beb5e71f92952e
yLeozinn <leozinhofilho35@gmail.com>
2026-01-28 03:09:20 -0300

    mineable

---
+++ src/main/java/br/com/leozin/modz/projetomodmine/ModBlocks.java
@@ -0,0 +1,34 @@
+package br.com.leozin.modz.projetomodmine;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredHolder;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.createBlocks(Projetomodmine.MODID);
+
+    // criação do bloco de tungsten ore (mesma dureza da pedra)
+    public static final DeferredHolder<Block, Block> TUNGSTEN_ORE = registerBlock("tungsten_ore",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).strength(3.0f).requiresCorrectToolForDrops()));
+
+    private static <T extends Block> DeferredHolder<Block, T> registerBlock(String name, Supplier<T> block) {
+        DeferredHolder<Block, T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    public static <T extends Block> void registerBlockItem(String name, DeferredHolder<Block, T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
---
+++ src/main/java/br/com/leozin/modz/projetomodmine/ModItems.java
@@ -0,0 +1,23 @@
+package br.com.leozin.modz.projetomodmine;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredHolder;
+
+public class ModItems {
+    // registrar os itens no jogo
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.createItems(Projetomodmine.MODID);
+
+    // tungstenio cru
+    public static final DeferredHolder<Item, Item> RAW_TUNGSTEN = ITEMS.register("raw_tungsten",
+            () -> new Item(new Item.Properties()));
+
+    //barra de tungstenio
+    public static final DeferredHolder<Item, Item> TUNGSTEN_INGOT = ITEMS.register("tungsten_ingot",
+            () -> new Item(new Item.Properties()));
+    // registra tudo na classe principal
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
